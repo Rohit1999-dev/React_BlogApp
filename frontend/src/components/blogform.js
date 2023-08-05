1ffc52af38de09b028c1a07247fdaba441c2bf23
abhinav7332 <rohit18@navgurukul.org>
2023-08-06 00:49:36 +0530

    Make changes in blog file

--- frontend/src/components/blogform.js
+++ frontend/src/components/blogform.js
@@ -49,10 +49,10 @@ function Blogform() {
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                             <li class="nav-item">
-                                <a class="nav-link" aria-current="page" to={"/home"} href="/home">Home</a>
+                                <p class="nav-link" aria-current="page" onClick={()=>{navigate('/home')}}>Home</p>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" to={"/blogform"} href="/blogform">Blog Form</a>
+                            <p class="nav-link" onClick={()=>{navigate('/blogform')}}>Blog Form</p>
                             </li>
                         </ul>
                     </div>
